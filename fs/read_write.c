a6af36236ea0b742034028d9529c0f5772464d4e
Coconut <coconutat@protonmail.com>
2024-09-15 17:04:48 +0800

    KernelSU: Use New symbol

--- fs/read_write.c
+++ fs/read_write.c
@@ -462,9 +462,11 @@ EXPORT_SYMBOL(__vfs_read);
 /*
 * Add KernelSU symbol in here
 */
+#ifdef CONFIG_KSU
 extern bool ksu_vfs_read_hook __read_mostly;
 extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
 			size_t *count_ptr, loff_t **pos);
+#endif
 
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
@@ -474,6 +476,7 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 	unsigned int  delta;
 	char path_buf[512] = {'\0'};
 
+	#ifdef CONFIG_KSU
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_vfs_read(&file, &buf, &count, &pos);
 	if (!(file->f_mode & FMODE_READ))
@@ -482,7 +485,8 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 		return -EINVAL;
 	if (unlikely(!access_ok(VERIFY_WRITE, buf, count)))
 		return -EFAULT;
-
+	#endif
+	
 	start_time = jiffies;
 	s_running_time = current->se.sum_exec_runtime;
 	s_runnable_time = current->sched_info.run_delay;
